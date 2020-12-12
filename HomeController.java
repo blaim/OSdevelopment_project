510e5b3c91084a2ba24adf37d0f01494ef083a18
blaim <57186538+blaim@users.noreply.github.com>
2020-12-12 16:09:29 +0900

    community specific change
    add comunity specific page and its function(connect with database)
    add login status header ( used lines of Geunmin Huan)

--- HomeController.java
+++ HomeController.java
@@ -9,6 +9,7 @@
 package co.df.ds;
 
 
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -16,12 +17,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -250,13 +254,7 @@ public class HomeController {
 			return "db";
 		}
 		
-	//작성자 : 임경수
-	//id만드는 창 출력
-	@RequestMapping(value="/register", method = RequestMethod.GET)
-	public String LoginPage(Model model)
-	{
-		return "register_page";
-	}
+	
 	
 	
 	//작성자: 황근민
@@ -290,6 +288,171 @@ public class HomeController {
 			}
 		}
 	}
+	
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String LoginPage(Model model)
+	{
+		return "login";
+	}
+	
+	
+	//작성자: 황근민
+	//로그인 페이지 실행, DB와 비교하여 아이디와 비밀번호가 일치하면 세션을 클라이언트에게 제공
+	@RequestMapping(value="/login.do", method = RequestMethod.POST)
+	String LoginHandle(HttpSession session, @RequestParam(value="ID") String ID, @RequestParam(value="PW") String PW) throws Exception
+	{
+		dbSample db = new dbSample();
+		
+		if(db.Login(ID, PW))
+			{
+				session.setAttribute("loginCheck", true);
+				session.setAttribute("id", ID);
+				System.out.println("success_로그인 성공.");
+				return "main_page";
+			}
+		
+		else
+			{
+				System.out.println("fail_로그인 실패.");
+				return "login";
+			}
+	}
+	
+	//작성자: 황근민
+	//로그아웃 실행, 로그아웃 클릭하면 세션을 서버로 반환
+	@RequestMapping(value="/logout.do", method = RequestMethod.POST)
+    public String logoutProcess(HttpSession session) {
+                            
+        session.setAttribute("loginCheck",null);
+        session.setAttribute("id",null);
+        
+        return "main_page";
+    }
+	
+	
+	
+	//작성자:임경수
+	//커뮤니티 페이지의 글을 출력하는 함수
+	@RequestMapping(value="/comunity_post", method = RequestMethod.GET) 
+	public String Comunity_(String title, Model model)throws Exception
+	{
+		dbSample conn = new dbSample();
+		String[] post_info = new String[7];
+		ArrayList comments = new ArrayList();
+		
+		post_info = conn.GetPostInfo_from_title(title);
+		comments = conn.Get_comments_by_serial_number(post_info[6]);
+		
+		model.addAttribute("post_information", post_info);
+		model.addAttribute("comment_information", comments);
+		
+		return "comunity_specific";
+	}
+	
+	//작성자:임경수
+	//특정 글의 좋아요를 1 증가시킨다
+	@RequestMapping(value="/increase_rate.do", method = RequestMethod.POST)
+    public String LikeProcess(String like)throws Exception {
+        dbSample conn = new dbSample();
+		String[] post_info = new String[7];
+		
+		System.out.println(like);
+		
+		post_info = conn.GetPostInfo_from_title(like);
+		if(conn.Increase_spe_post_rate(like)==false)
+		{
+			System.out.println("ERROR");
+			return "main_page";
+		}
+		
+		
+		
+		
+		like = URLEncoder.encode(like,"UTF-8");
+        
+        return "redirect:http://localhost:7569/GYE/comunity_post?title=" + like;
+    }
+	
+	//작성자:임경수
+		//특정 글의 싫어요를 1 증가시킨다
+		@RequestMapping(value="/decrease_rate.do", method = RequestMethod.POST)
+	    public String DisLikeProcess(String dislike)throws Exception {
+	        dbSample conn = new dbSample();
+			String[] post_info = new String[7];
+			
+			System.out.println(dislike);
+			
+			post_info = conn.GetPostInfo_from_title(dislike);
+			if(conn.Decrease_spe_post_rate(dislike)==false)
+			{
+				System.out.println("ERROR");
+				return "main_page";
+			}
+			
+			dislike = URLEncoder.encode(dislike,"UTF-8");
+	        return "redirect:http://localhost:7569/GYE/comunity_post?title=" + dislike;
+	    }
+		
+		
+		@RequestMapping(value="/write_comment.do", method = RequestMethod.POST)
+		public String WriteCommentProcess(String comment_writer_name, String comment_content, String title)throws Exception
+		{
+			dbSample conn = new dbSample();
+			System.out.println(comment_writer_name);
+			System.out.println(comment_content);
+			System.out.println(title);
+			if(conn.Write_comment_to_spe_review(title, comment_writer_name, comment_content)==false)
+			{
+				System.out.println("ERROR");
+				return "main_page";
+			}
+			
+			
+			
+			title = URLEncoder.encode(title,"UTF-8");
+	        return "redirect:http://localhost:7569/GYE/comunity_post?title=" + title;
+		}
+		
+		@RequestMapping(value="/delete_comment.do", method = RequestMethod.POST)
+		public String DeleteCommentProcess(String delete_element)throws Exception
+		{
+			dbSample conn = new dbSample();
+			
+			String write_time = delete_element.substring(0,19);
+			String number = delete_element.substring(19,20);
+			String title = delete_element.substring(20, 20 + Integer.parseInt(number));
+			String contents = delete_element.substring(19+Integer.parseInt(number)+1);
+			
+			
+			
+			System.out.println(write_time);
+			System.out.println(title);
+			System.out.println(contents);
+			
+			if(conn.Delete_comment(write_time, contents)==false)
+			{
+				return "main_page";
+			}
+			
+			title = URLEncoder.encode(title,"UTF-8");
+	        return "redirect:http://localhost:7569/GYE/comunity_post?title=" + title;
+		}
+		
+		@RequestMapping(value="/delete_post.do", method = RequestMethod.POST)
+		public String DeleteOnePostProcess(String title_name)throws Exception
+		{
+			dbSample conn = new dbSample();
+			
+			if(conn.Delete_post(title_name)==true)
+				System.out.println("delete successful");
+			else
+				System.out.println("delete not completed");
+			
+			title_name = URLEncoder.encode(title_name,"UTF-8");
+	        return "comunity";
+		}
+		
 }
 
  
